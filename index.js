8b269080d40094c9d740ece506ecc093637b051a
Md Sohrab Ansari <ansarisohrab75@gmail.com>
2023-01-24 16:39:01 +0530

    code

--- index.js
+++
@@ -1,8 +0,0 @@
-const fs= require('fs');
-
-// fs.writeFileSync("read.txt", "hello world");
-
-//fs.writeFileSync("read.txt", "Hello NodsJs World");
-//fs.appendFileSync("read.txt", "Hello NodsJs World 22");
-
-fs.renameSync("read.txt", "readwrite.txt");
